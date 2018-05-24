fc409fbb69870185ad68a8a1ec5f903002056f1d
Yifan Pan <4anpan@gmail.com>
2018-05-24 10:12:44 +0800

    use es6 module directly export

--- src/my-recompose/index.js
+++ src/my-recompose/index.js
@@ -1,15 +1,6 @@
-import withState from './with-state';
-import withHandlers from './with-handlers';
-import withProps from './with-props';
-import mapProps from './map-props';
-import compose from './compose';
-import setDisplayName from './set-display-name';
-
-export {
-  withState,
-  withHandlers,
-  withProps,
-  mapProps,
-  setDisplayName,
-  compose
-}
+export { default as withState } from './with-state';
+export { default as withHandlers } from './with-handlers';
+export { default as withProps } from './with-props';
+export { default as mapProps } from './map-props';
+export { default as compose } from './compose';
+export { default as setDisplayName } from './set-display-name';
