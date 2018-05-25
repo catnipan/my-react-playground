66fd8ee34644090609951d69f9e793b2e27d16af
Yifan Pan <4anpan@gmail.com>
2018-05-25 17:31:07 +0800

    optimize with-handler? but does it really work???

--- src/App.js
+++ src/App.js
@@ -5,29 +5,54 @@ import './App.css';
 import { withState, withHandlers, withProps, mapProps, compose, setDisplayName } from './my-recompose';
 
 export default compose(
-  setDisplayName('MyNameHHH'),
-  mapProps({
-    start: ({ startNumber }) => startNumber + 1
-  }),
   withState('counter', 'setCounter', 0),
-  withState('max', '', 100),
+  withState('max', '', 5),
   withHandlers({
-    increment: ({setCounter}) => () => setCounter(n => n + 1),
+    increment: ({setCounter, counter, max}) => () => {
+      if(counter >= max) return;
+      setCounter(n => n + 1);
+    },
     decrement: ({setCounter}) => () => setCounter(n => n - 1),
-    reset: ({setCounter}) => () => setCounter(0)
-  }),
-  withProps({
-    counterUntilMax: ({counter, max}) => Math.max(0, max - counter)
+    reset: ({setCounter}) => () => setCounter(0),
   })
-)(function Counter({ start, counter, counterUntilMax, increment, decrement, reset }){
+)(function Counter({ counter, decrement, increment, reset }){
   return (
     <div>
-      outerProps: { start } <br/>
-      Count: { counter } <br/>
-      Count Until Max: { counterUntilMax } <br/>
+      Count: {counter}
       <button onClick={increment}>Increment</button>
       <button onClick={decrement}>Decrement</button>
       <button onClick={reset}>Reset</button>
     </div>
   )
 })
+
+// export default compose(
+//   setDisplayName('MyNameHHH'),
+//   mapProps({
+//     start: ({ startNumber }) => startNumber + 1
+//   }),
+//   withState('counter', 'setCounter', 0),
+//   withState('max', '', 10),
+//   withHandlers({
+//     increment: ({setCounter}) => () => {
+//       // if(counter >= max) return;
+//       setCounter(n => n + 1);
+//     },
+//     decrement: ({setCounter}) => () => setCounter(n => n - 1),
+//     reset: ({setCounter}) => () => setCounter(0)
+//   }),
+//   withProps({
+//     counterUntilMax: ({counter, max}) => Math.max(0, max - counter)
+//   })
+// )(function Counter({ start, counter, counterUntilMax, increment, decrement, reset }){
+//   return (
+//     <div>
+//       outerProps: { start } <br/>
+//       Count: { counter } <br/>
+//       Count Until Max: { counterUntilMax } <br/>
+//       <button onClick={increment}>Increment</button>
+//       <button onClick={decrement}>Decrement</button>
+//       <button onClick={reset}>Reset</button>
+//     </div>
+//   )
+// })
--- src/my-recompose/with-handlers.js
+++ src/my-recompose/with-handlers.js
@@ -1,22 +1,26 @@
 import React from 'react';
+import shallowEqual from 'shallowequal';
 import { getDisplayName } from './util';
 
 export default (handlerCreators) => WrappedComponent => {
-  class WithHandlersComponent extends React.Component{
-    constructor(props){
-      super(props);
-      this.generatedHandlers = {};
+  var beforeProps = {};
+  const generatedHandlers = {};
+  function WithHandlersComponent(props){
+    if(!shallowEqual(beforeProps, props)){
       for(var handlerName in handlerCreators){
-        this.generatedHandlers[handlerName] = handlerCreators[handlerName](props);
+        generatedHandlers[handlerName] = handlerCreators[handlerName](props);
       }
+      beforeProps = { ...props };
     }
-    render(){
-      return React.createElement(WrappedComponent, {
-        ...this.props,
-        ...this.generatedHandlers
-      })
-    }
+    return React.createElement(WrappedComponent, {
+      ...props,
+      ...generatedHandlers
+    })
   }
   WithHandlersComponent.displayName = `withHandlers(${getDisplayName(WrappedComponent)})`;
   return WithHandlersComponent;
 }
+
+// Takes an object map of handler creators or a factory function. These are higher-order functions that accept a set of props and return a function handler:
+// This allows the handler to access the current props via closure, without needing to change its signature.
+// Handlers are passed to the base component as immutable props, whose identities are preserved across renders. This avoids a common pitfall where functional components create handlers inside the body of the function, which results in a new handler on every render and breaks downstream shouldComponentUpdate() optimizations that rely on prop equality. This is the main reason to use withHandlers to create handlers instead of using mapProps or withProps, which will create new handlers every time when it get updated.
